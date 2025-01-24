07f367145c71c73ededd1fbae89848b10a9957f5
Rico Miles Quiblat <120778516+ricomiles@users.noreply.github.com>
2025-01-25 00:47:44 +0800

    chore: remove hardcoded cost models and update blaze and utxorpc dependencies (#27)
    * fix: remove hardcoded cost models and incorrect params parsing
    
    * chore: update blaze and utxorpc dependencies

--- src/u5c.ts
+++ src/u5c.ts
@@ -229,7 +229,7 @@ export class U5C extends Provider {
     tx.body().inputs().setValues(finalInputs);
     tx.body().setOutputs(finalOutputs);
     const unevaluatedRedeemers = tx.witnessSet().redeemers()?.values()!;
-    
+
     const report = await this.submitClient.evalTx(fromHex(tx.toCbor()));
     const evalResult = report.report[0].chain.value?.redeemers!;
 
@@ -243,9 +243,9 @@ export class U5C extends Provider {
           BigInt(evalResult[i].exUnits?.memory!),
           BigInt(evalResult[i].exUnits?.steps!)
         )
-      ))
+      ));
     }
-    
+
     const finalRedeemers = Redeemers.fromCore([]);
     finalRedeemers.setValues(redeemers);
     return finalRedeemers;
@@ -358,27 +358,11 @@ export class U5C extends Provider {
         )
         .set(
           PlutusLanguageVersion.V3,
-          [100788, 420, 1, 1, 1000, 173, 0, 1, 1000, 59957, 4, 1, 11183, 32, 201305, 8356, 4,
-            16000, 100, 16000, 100, 16000, 100, 16000, 100, 16000, 100, 16000, 100, 100, 100,
-            16000, 100, 94375, 32, 132994, 32, 61462, 4, 72010, 178, 0, 1, 22151, 32, 91189,
-            769, 4, 2, 85848, 123203, 7305, -900, 1716, 549, 57, 85848, 0, 1, 1, 1000, 42921,
-            4, 2, 24548, 29498, 38, 1, 898148, 27279, 1, 51775, 558, 1, 39184, 1000, 60594, 1,
-            141895, 32, 83150, 32, 15299, 32, 76049, 1, 13169, 4, 22100, 10, 28999, 74, 1,
-            28999, 74, 1, 43285, 552, 1, 44749, 541, 1, 33852, 32, 68246, 32, 72362, 32, 7243,
-            32, 7391, 32, 11546, 32, 85848, 123203, 7305, -900, 1716, 549, 57, 85848, 0, 1,
-            90434, 519, 0, 1, 74433, 32, 85848, 123203, 7305, -900, 1716, 549, 57, 85848, 0, 1,
-            1, 85848, 123203, 7305, -900, 1716, 549, 57, 85848, 0, 1, 955506, 213312, 0, 2,
-            270652, 22588, 4, 1457325, 64566, 4, 20467, 1, 4, 0, 141992, 32, 100788, 420, 1, 1,
-            81663, 32, 59498, 32, 20142, 32, 24588, 32, 20744, 32, 25933, 32, 24623, 32,
-            43053543, 10, 53384111, 14333, 10, 43574283, 26308, 10, 16000, 100, 16000, 100,
-            962335, 18, 2780678, 6, 442008, 1, 52538055, 3756, 18, 267929, 18, 76433006, 8868,
-            18, 52948122, 18, 1995836, 36, 3227919, 12, 901022, 1, 166917843, 4307, 36, 284546,
-            36, 158221314, 26549, 36, 74698472, 36, 333849714, 1, 254006273, 72, 2174038, 72,
-            2261318, 64571, 4, 207616, 8310, 4, 1293828, 28716, 63, 0, 1, 1006041, 43623, 251,
-            0, 1, 100181, 726, 719, 0, 1, 100181, 726, 719, 0, 1, 100181, 726, 719, 0, 1,
-            107878, 680, 0, 1, 95336, 1, 281145, 18848, 0, 1, 180194, 159, 1, 1, 158519, 8942,
-            0, 1, 159378, 8813, 0, 1, 107490, 3298, 1, 106057, 655, 1, 1964219, 24520, 3]
-          ,
+          rpcPParams.costModels?.plutusV3?.values.map((v) =>
+            Number(v.toString()),
+          ) ??
+            hardCodedProtocolParams.costModels.get(PlutusLanguageVersion.V3) ??
+            [],
         ),
       maxBlockBodySize: Number(rpcPParams.maxBlockBodySize),
       maxBlockHeaderSize: Number(rpcPParams.maxBlockHeaderSize),
@@ -395,9 +379,9 @@ export class U5C extends Provider {
       stakeKeyDeposit: Number(rpcPParams.stakeKeyDeposit),
       poolRetirementEpochBound: Number(rpcPParams.poolRetirementEpochBound),
       desiredNumberOfPools: Number(rpcPParams.desiredNumberOfPools),
-      poolInfluence: `${rpcPParams.poolInfluence?.numerator}/${rpcPParams.poolInfluence?.denominator}`,
-      monetaryExpansion: `${rpcPParams.monetaryExpansion?.numerator}/${rpcPParams.monetaryExpansion?.denominator}`,
-      treasuryExpansion: `${rpcPParams.treasuryExpansion?.numerator}/${rpcPParams.treasuryExpansion?.denominator}`,
+      poolInfluence: `${rpcPParams.poolInfluence?.numerator! / rpcPParams.poolInfluence?.denominator!}`,
+      monetaryExpansion: `${rpcPParams.monetaryExpansion?.numerator! / rpcPParams.monetaryExpansion?.denominator!}`,
+      treasuryExpansion: `${rpcPParams.treasuryExpansion?.numerator! / rpcPParams.treasuryExpansion?.denominator!}`,
       protocolVersion: {
         minor: Number(rpcPParams.protocolVersion?.minor),
         major: Number(rpcPParams.protocolVersion?.major),
